a246af0aada299ffc28bcc06cd2c6928d88ab689
syzon <carlo.1990@hotmail.de>
2017-03-15 22:08:15 +0100

    test123

--- src/de/buderus/driver/heating/BuderusHeatingDriver.java
+++ src/de/buderus/driver/heating/BuderusHeatingDriver.java
@@ -19,6 +19,8 @@ public class BuderusHeatingDriver {
 
     /*Teeeeeeeeeeest*/
 
+    //Test von Karle xD
+
     public BuderusHeatingDriver(String productSerialNumber){
 
         //TODO: Initialize and connect to heating!
